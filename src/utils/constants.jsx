06b2df9366fd7147d83e9eddbd073114a4bb436b
pravesh1731 <706990pk@gmail.com>
2025-12-11 15:04:14 +0530

    dynamic BaseUrl is added for production and development

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,9 @@
+import e from "cors";
 import React from "react";
 
-export const BaseUrl = "/api";
+// production
+// export const BaseUrl = "/api";
+
+// development
+export const BaseUrl = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+
